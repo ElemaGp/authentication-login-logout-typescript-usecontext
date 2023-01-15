b6b683615b601bc01cf2de2b3b6ce7bad767cc82
ElemaGp <elemagodspower@gmail.com>
2023-01-15 18:32:00 +0100

    feat: frontend UI for the signup page and login page created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
