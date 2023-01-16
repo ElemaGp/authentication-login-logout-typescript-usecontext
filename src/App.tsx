978b8fc9775a0ee9b7f426e06add68d1d97d00bf
ElemaGp <elemagodspower@gmail.com>
2023-01-16 09:42:27 +0100

    feat: added routing to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="login" element={<Login />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
